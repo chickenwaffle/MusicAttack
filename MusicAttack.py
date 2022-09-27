f1212779a613051fb8f06c4d2b47e7fdec13101e
kornflakes <msw102394@gmail.com>
2022-09-27 01:24:45 -0400

    IPU Class created
    Object oriented programming applied.  Now there is one IPU object created and used throughout the program.  This eliminates the need to start and stop multiple streams in the program, rather than using one object that contains the stream and everything it needs.

--- MusicAttack.py
+++ MusicAttack.py
@@ -17,73 +17,215 @@ from os.path import exists
 import time     #time.sleep()
 import json
 
-FSAMP = 48000       # Sampling frequency in Hz
-FRAME_SIZE = 48000//60   # How many samples per frame? as much as we can get in 1/60th of a second
-FRAMES_PER_FFT = 1  # FFT takes average across how many frames? (number of frames to delay for better accuracy)
+# Input Processing Utilities
+#
+# 
+class IPU:
+    __pyaud = None
+    stream = None
 
-SAMPLES_PER_FFT = FRAME_SIZE*FRAMES_PER_FFT
+    DEFAULT_FSAMP = 48000
+    DEFAULT_FRAME_SIZE = 800
+    DEFAULT_FRAMES_PER_FFT = 1
+    DEFAULT_MIC_INDEX = 0
+    NOTE_NAMES = 'C C# D D# E F F# G G# A A# B'.split()
+    MONO = 1
 
-######################################################################
-# For printing out notes
+    fsamp = 0
+    frame_size = 0
+    frames_per_fft = 0
+    mic_index = 0
+    samples_per_fft = 0
+    amplitude = 0.0
+    buf = None
+    note = 0
+    note_name = ""
+    fourier_array = None
+    piano_frequencies = None
 
-NOTE_NAMES = 'C C# D D# E F F# G G# A A# B'.split()
+    def __init__(self):
+        self.__pyaud = pyaudio.PyAudio()
+        self.fsamp = self.DEFAULT_FSAMP
+        self.frame_size = self.DEFAULT_FRAME_SIZE
+        self.frames_per_fft = self.DEFAULT_FRAMES_PER_FFT
+        self.samples_per_fft = self.frame_size * self.frames_per_fft
+        self.mic_index = self.DEFAULT_MIC_INDEX
+
+        self.fourier_array = (-2 * np.pi / self.fsamp) * np.arange(self.samples_per_fft)
+        self.piano_frequencies = [self.number_to_freq(i) for i in np.arange(1, 89)]
+    
+    
+    def get_pyaudio(self):
+        return(self.__pyaud)
+
+    def get_stream(self):
+        return(self.stream)
+
+    def get_sample_size(self):
+        return(self.sample_size)
+    
+    def get_amplitude(self):
+        return(self.amplitude)
+    
+    def get_sampling_rate(self):
+        return(self.sampling_rate)  
+
+    def get_note(self):
+        return(self.note)
+
+    def get_note_name(self):
+        return(self.note_name)
+
+    def get_mic_index(self):
+        return self.mic_index
+    
+    def set_sample_size(self, size):
+        self.sample_size=size
+        self.start()
+    
+    def set_sampling_rate(self, rate):
+        self.sampling_rate=rate  
+        self.start()
 
 
-######################################################################
-# These three functions are based upon this very useful webpage:
-# https://en.wikipedia.org/wiki/Piano_key_frequencies
-def freq_to_number(f):
-    return 12*np.log2(f/440.0) + 49
+    def start(self):
+        if(self.stream):
+            self.stop()  
+        if(not self.mic_index):
+            self.mic_index=int(self.get_pyaudio().get_default_input_device_info()["index"])
+            #print(self.mic_index)
+            self.set_sampling_rate(int(self.get_pyaudio().get_device_info_by_index(self.mic_index)["defaultSampleRate"]))
+            #print(self.getSamplingRate())
+        try:
+            self.buf = np.zeros(self.samples_per_fft, dtype=np.float32)
+            self.stream=self.get_pyaudio().open(format=pyaudio.paInt16,
+                                               channels=self.MONO,
+                                               input_device_index=int(self.mic_index),
+                                               rate=self.fsamp,
+                                               input=True,
+                                               frames_per_buffer=self.frame_size)
+        except OSError:
+            print("Error: Microphone did not properly initialize.\nPlease check if you are using correct microphone index and try again.\n")
+            exit()
+
+    def stop(self):
+        self.stream.stop_stream()
+        self.stream.close() 
+
+    def implode(self):   
+        self.stop()
+        self.get_pyaudio().terminate()
+    
+    def toggle(self):
+        if(self.stream):
+            self.stop()
+        else:
+            self.start()
+
+    ######################################################################
+    # These three functions are based upon this very useful webpage:
+    # https://en.wikipedia.org/wiki/Piano_key_frequencies
+    def freq_to_number(self, f):
+        return 12*np.log2(f/440.0) + 49
 
 
-def number_to_freq(n):
-    return 440 * 2.0**((n-49)/12.0)
+    def number_to_freq(self, n):
+        return 440 * 2.0**((n-49)/12.0)
 
 
-def note_name(n):
-    return NOTE_NAMES[(n - 4) % 12] + str(int((n + 8) / 12))
-
-######################################################################
-fourier_array = (-2 * np.pi / FSAMP) * np.arange(SAMPLES_PER_FFT)
-piano_frequencies = [number_to_freq(i) for i in np.arange(1, 89)]
+    def note_to_note_name(self, n):
+        return self.NOTE_NAMES[(n - 4) % 12] + str(int((n + 8) / 12))
 
 
-def discrete_fourier_transform(f, samples):
-    fourier_array_times_freq = f * fourier_array
-    #return np.absolute(np.sum(samples * np.exp(1j * fourier_array_times_freq))) / samples.size
-    x = np.sum(samples * np.cos(fourier_array_times_freq))
-    y = np.sum(samples * np.sin(fourier_array_times_freq))
-    return np.sqrt(x*x+y*y) / samples.size
+
+    ######################################################################
 
 
-def get_dominant_pitch(samples):
-    response = [discrete_fourier_transform(f, samples) for f in piano_frequencies]
-    max_index = np.argmax(response)
-    return piano_frequencies[max_index], response[max_index]
-######################################################################
 
-# Calculate note returns position on an 88-key piano.
-# An A440 will return the value 49.
-# Result can be passed as an arg thru function note_name() to get note name, i.e. A4
-def calculate_note(stream, buf):
-    # Shift the buffer down and new data in
-    buf[:-FRAME_SIZE] = buf[FRAME_SIZE:]
-    buf[-FRAME_SIZE:] = np.frombuffer(stream.read(FRAME_SIZE), np.int16)
+    def discrete_fourier_transform(self, f, samples):
+        fourier_array_times_freq = f * self.fourier_array
+        #return np.absolute(np.sum(samples * np.exp(1j * fourier_array_times_freq))) / samples.size
+        x = np.sum(samples * np.cos(fourier_array_times_freq))
+        y = np.sum(samples * np.sin(fourier_array_times_freq))
+        return np.sqrt(x*x+y*y) / samples.size
 
-    # Run the DFFT on the buffer
-    dominant_frequency, amplitude = get_dominant_pitch(buf)# * window)
 
-    # Return 0 if sound isn't loud enough.
-    #TODO: Move this to somewhere else so that testing can be done properly.
-    if amplitude < 300:
-        return 0
+    def get_dominant_pitch(self, samples):
+        response = [self.discrete_fourier_transform(f, samples) for f in self.piano_frequencies]
+        max_index = np.argmax(response)
+        return self.piano_frequencies[max_index], response[max_index]
+    ######################################################################
 
-    # Get note number and nearest note
-    n = freq_to_number(dominant_frequency)
-    n0 = int(round(n))
+    def get_microphone_list(self):
+        retval={}
+        adict=None
+        for devindex in range(self.get_pyaudio().get_device_count()):
+            adict=self.get_pyaudio().get_device_info_by_index(devindex)
+            if(bool(int(adict["maxInputChannels"]))):
+                retval[adict["name"]]=adict["index"]
+        return(retval)     
 
-    return n0
+    def get_microphone_name(self):
+        # Cancel if microphone is not set
+        if self.mic_index == 0:
+            return("NULL")
 
+        retval={}
+        adict=self.get_microphone_list()
+
+        for key, value in adict.items():
+            if value == self.mic_index:
+                return(key)
+
+    def set_microphone(self, index):
+        self.mic_index=index
+        if(self.stream):
+            self.stop()
+
+    # If no argument is given to set_microphone, then
+    # interactively prompt the user to select a microphone from a list
+    def set_microphone(self):
+        inputs = self.get_microphone_list()
+        print("#\tInput\n========================================")
+        for key, value in inputs.items():
+            print(str(value) + "\t" + key)
+
+        self.mic_index = input("Select a microphone #: ")
+
+    # Calculate note returns position on an 88-key piano.
+    # An A440 will return the value 49.
+    # Result can be passed as an arg thru function note_name() to get note name, i.e. A4
+    def calculate_note(self):
+        if(self.stream):
+            # Shift the buffer down and new data in
+            self.buf[:-self.frame_size] = self.buf[self.frame_size:]
+            self.buf[-self.frame_size:] = np.frombuffer(self.stream.read(self.frame_size), np.int16)
+
+            # Run the DFFT on the buffer
+            self.dominant_frequency, self.amplitude = self.get_dominant_pitch(self.buf)# * window)
+
+            # Return 0 if sound isn't loud enough.
+            #TODO: Move this to somewhere else so that testing can be done properly.
+            if self.amplitude < 300:
+                self.note_name = 0
+                return
+            else:
+                # Get note number and nearest note
+                n = self.freq_to_number(self.dominant_frequency)
+                n0 = int(round(n))
+                self.note = n0
+                self.note_name = self.note_to_note_name(n0)
+
+
+
+    def test(self):
+        self.start()
+        while(self.stream.is_active()):
+            try:
+                self.calculate_note()
+                print("\rMIDI:\t{}\t\tNote:\t{} \t\tAmpl:\t{}".format(self.note, self.note_name, self.amplitude), end="")
+            except KeyboardInterrupt:
+                exit()
 ######################################################################
 # Functions relating to Panel Attack and config loading/creation
 
@@ -129,51 +271,22 @@ def mode(ls):
     keys = [key for key in counts.keys() if counts[key] == max(counts.values())] 
     return(keys[0])
 
-def get_microphone_list():
-    retval={}
-    adict=None
-    for devindex in range(pyaudio.PyAudio().get_device_count()):
-      adict=pyaudio.PyAudio().get_device_info_by_index(devindex)
-      if(bool(int(adict["maxInputChannels"]))):
-        retval[adict["name"]]=adict["index"]
-    return(retval)     
-
-
-# Interactively prompt the user to select a microphone from a list
-def set_microphone():
-    inputs = get_microphone_list()
-    print("#\tInput\n========================================")
-    for key, value in inputs.items():
-        print(str(value) + "\t" + key)
-
-    mic_index = input("Select a microphone #: ")
-
-    return mic_index
-
-
 # Contains code to interactively create config.json
-def __create_config(configfile, panelattack_keys):
+def __create_config(ipu, configfile, panelattack_keys):
     new_config = {}
-    new_config["mic_index"] = int(set_microphone())
+
+    ipu.set_microphone()
+    new_config["mic_index"] = int(ipu.get_mic_index())
 
     # Create an index keys inside config dictionary
     new_config["keys"] = {}
 
-    key_list = list(panelattack_keys.values())
-
     # Used to iterate thru key_list
     # Since Python doesn't like to convert dict values into lists and vice versa
+    key_list = list(panelattack_keys.values())
     pos = 0
 
-    buf = np.zeros(SAMPLES_PER_FFT, dtype=np.float32)
-    stream = pyaudio.PyAudio().open(format=pyaudio.paInt16,
-                                    channels=1,
-                                    input_device_index=new_config["mic_index"],
-                                    rate=FSAMP,
-                                    input=True,
-                                    frames_per_buffer=FRAME_SIZE)
-
-    stream.start_stream()
+    ipu.start()
 
     # For each button, record 15 samples to find most
     # commonly occuring note.  Returns all keys as dict when done.
@@ -184,7 +297,8 @@ def __create_config(configfile, panelattack_keys):
 
         while (i < len(recorded_notes)):
             print("\r" + str(pa_key) + "                              ", end='')
-            recorded_notes[i] = calculate_note(stream, buf)
+            ipu.calculate_note()
+            recorded_notes[i] = ipu.get_note_name()
 
             if recorded_notes[i] == 0:
                 i = 0
@@ -195,21 +309,18 @@ def __create_config(configfile, panelattack_keys):
             time.sleep(0.1)
 
         note_mode = mode(recorded_notes)
-        note = note_name(note_mode)
-        new_config["keys"][note] = key_list[pos]
+        new_config["keys"][note_mode] = key_list[pos]
 
-        print("\r" + str(pa_key) + "\tBound freq " + note + " to button \'" + key_list[pos] + "\'")
+        print("\r" + str(pa_key) + "\tBound note " + note_mode + " to button \'" + key_list[pos] + "\'")
 
         pos += 1
-
-    stream.stop_stream()
-
     
     option = input("Save keybindings? (Y/n) ")
     if option.lower() == "y" or option.lower() == "yes" or option == "":
         with open(configfile,"a") as f:
             json.dump(new_config, f)
 
+    ipu.stop()
     return new_config
 
 # Attempt to load config.json. If fails, 
@@ -239,7 +350,7 @@ def load_config(panelattack_keys):
             exit()
 
         if option.lower() == "y" or option.lower() == "yes" or option == "":
-            return __create_config(configfile, panelattack_keys)
+            return __create_config(ipu, configfile, panelattack_keys)
 
 
         else:
@@ -248,25 +359,16 @@ def load_config(panelattack_keys):
 
 # Called in main() as the loop that translates notes to key inputs to Panel Attack
 # Argument note_name_to_key takes dictionary.
-def panel(config):
-
-    buf = np.zeros(SAMPLES_PER_FFT, dtype=np.float32)
-    stream = pyaudio.PyAudio().open(format=pyaudio.paInt16,
-                                    channels=1,
-                                    input_device_index=config["mic_index"],
-                                    rate=FSAMP,
-                                    input=True,
-                                    frames_per_buffer=FRAME_SIZE)
-
-    stream.start_stream()
+def panel(ipu, config):
+    ipu.start()
 
     is_pressed = False
 
-    while stream.is_active():
-        freq = calculate_note(stream, buf)
+    while ipu.get_stream().is_active():
+        freq = ipu.calculate_note()
 
         try:
-            kbpress = config["keys"][ note_name(freq) ]
+            kbpress = config["keys"][ ipu.get_note_name() ]
             if not is_pressed:
                 keyboard.send(kbpress)
                 print(str(kbpress))
@@ -284,12 +386,17 @@ def panel(config):
 
 # Ok, ready to go now.
 
+def test(ipu):
+   ipu.test() 
 
-def main():
+def main(ipu):
     panelattack_keys = get_panelattack_keys()
     config = load_config(panelattack_keys)
 
-    panel(config)
+    panel(ipu, config)
 
 if __name__ == "__main__":
-    main()
+    ipu = IPU()
+
+    test(ipu)
+    #main(ipu)
